75ec8205f86409b2b348fc113b3be26f63fec11a
Alexander <arkhomych.lit@gmail.com>
2021-07-01 17:08:01 +0300

    Completed task 1

---
+++ task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Task1(string input)
+        {
+            int number = 0;
+            try
+            {
+                number = Convert.ToInt32(input);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введенная строка не является числом.");
+            }
+
+            if (number > 7)
+                Console.WriteLine("Привет");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string input = Console.ReadLine();
+            Task1(input);
+            Console.ReadKey();
+        }
+    }
+}
