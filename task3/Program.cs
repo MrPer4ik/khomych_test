efe31e4f986d7593245dcdbcc4916ae98ce72634
Alexander Khomych <arkhomych.lit@gmail.com>
2021-07-01 18:25:53 +0300

    Completed task 3

---
+++ task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Task3(string input)
+        {
+            string[] str_numbers = input.Split(' ');
+            for (int i=0; i<str_numbers.Length; i++)
+            {
+                int number = Convert.ToInt32(str_numbers[i]);
+                if (number % 3 == 0)
+                    Console.Write("{0} ", number);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числовой массив (через пробел): ");
+            string input = Console.ReadLine();
+            Task3(input);
+            Console.ReadKey();
+        }
+    }
+}
