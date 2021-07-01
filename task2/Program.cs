fe7f338b7db684fe46d9eac6d3d84d0193cdaa43
Alexander Khomych <arkhomych.lit@gmail.com>
2021-07-01 17:52:13 +0300

    Completed task 2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Task2(string input)
+        {
+            if (input == "Вячеслав")
+                Console.WriteLine("Привет, {0}", input);
+            else
+                Console.WriteLine("Нет такого имени");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string input = Console.ReadLine();
+            Task2(input);
+            Console.ReadKey();
+        }
+    }
+}
